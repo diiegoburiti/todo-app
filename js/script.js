e98e173f2dc9a4490888b9c92663567ebd2f4ed2
Diego Buriti <ns.diego97@gmail.com>
2020-05-31 19:44:30 -0300

    Added count to list.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]');
+
+const listDisplayContainer = document.querySelector('[data-list-display-container]');
+const listCountElement = document.querySelector('[data-list-count]');
+const listTitleElement = document.querySelector('[data-list-title]');
+const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.getElementById('task-template');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.list';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -24,6 +31,12 @@ listsContainer.addEventListener('click', (event) => {
   }
 })
 
+deleteListButton.addEventListener('click', (event) => {
+  lists = lists.filter(list => list.id !== selectedListId);
+  selectedListId = null
+  saveAndRender();
+})
+
 function saveAndRender() {
   save();
   render();
@@ -40,22 +53,55 @@ function createList(name) {
 
 function render() {
   clearElement(listsContainer)
+  renderList();
+  const selectList = lists.find(list => list.id === selectedListId)
+  if (selectedListId === null) {
+    listDisplayContainer.style.display = 'none';
+  } else {
+    listDisplayContainer.style.display = 'block';
+    listTitleElement.innerText = selectList.name;
+    rederTaskCount(selectList);
+    clearElement(tasksContainer);
+    renderTasks(selectList);
+  }
+}
+
+function renderTasks(selectList) {
+  selectList.tasks.forEach(task => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector('checkbox');
+    checkbox.id = task.id
+    checkbox.checke = task.complete
+    const label = taskElement.querySelector('label');
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  });
+}
+
+function rederTaskCount(selectList) {
+  const incompleteTasks = selectList.tasks.filter(task => !task.complete).length
+  const taskString = incompleteTasks === 1 ? 'task' : 'tasks';
+  listCountElement.innerText = `${incompleteTasks} ${taskString} remaning`;
+}
+function renderList() {
   lists.forEach((list) => {
     const listElement = document.createElement('li');
     listElement.dataset.listId = list.id;
     listElement.classList.add('list-name');
     listElement.innerText = list.name;
     if (list.id === selectedListId) {
-      listElement.classList.add('active-list');     
+      listElement.classList.add('active-list');
     }
     listsContainer.appendChild(listElement);
   });
+}
 
-  function clearElement(element) {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild)
-    }
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
   }
 }
+
 
 render();
